49c7eb294e7d389de2850801e8e98fe91a7b4185
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-04 20:18:59 +0530

    don

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
+// src/redux/store.js
 import { createStore } from "redux";
-import reducer from "./reducer";
+import rootReducer from "./reducers";
 
-const store = createStore(reducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
